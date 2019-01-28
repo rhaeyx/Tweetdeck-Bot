f772ccb1da2126515ecdb0922441f8438ebcaed9
rhaeyx <43877303+rhaeyx@users.noreply.github.com>
2019-01-28 18:24:39 +0800

    main file
    file to run, config and enjoy

--- twitter.py
+++ twitter.py
@@ -1,18 +1,34 @@
 """
     This file works with TweetBot.py
 
-    bot.TwitterBotStart(text file, time a, time b)
+    TweetDeck-Bot project by rhaeyx.
+    https://github.com/rhaeyx/Tweetdeck-Bot
 """
 import TweetBot
 
-tweetbot = TweetBot.TweetBot(26)
-# bot.TwitterBotStart('tweets2.txt', '3', '4')
+bot = TweetBot.TweetBot()
 
-followbot = TweetBot.FollowBot()
-followbot.follow()
+# Set the name to your .txt file
+file_name = 'tweets.txt'
 
 """
-Finished files
-1. tweets.txt Jan - 24
-2.
+    bot.start(
+                file_name, # File name
+                [(12,30,'pm'),  # (hour, min, am/pm)
+                (3,30,'pm'),    # Add more or take away some
+                (4,30,'pm'),    # totally up to you
+                (5,30,'pm'),
+                (6,30,'pm'),
+                (7,30,'pm'),
+                (8,30,'pm')],
+                (1,31)          # Starting date (month, day)
+            )
 """
+
+bot.start(
+            file_name,  
+            [(12, 30, 'pm'),  
+            (3, 30, 'pm'),    
+            (4, 30, 'pm')],
+            (11, 8)          
+        )
