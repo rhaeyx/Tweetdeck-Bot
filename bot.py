1578e85a874ca3ccd95d6cd08d5fcedd1b64fcac
rhaeyx <43877303+rhaeyx@users.noreply.github.com>
2019-02-01 13:42:27 +0800

    new main file
    changed contents

---
+++ bot.py
@@ -0,0 +1,35 @@
+"""
+    tweetdeck_bot.py, a project by rhaeyx
+    Please consider giving this repo a star. 
+    https://github.com/rhaeyx/Tweetdeck-Bot
+
+    Contents:
+        tweetdeck_bot.py - source code
+        bot.py(this) - file to execute
+        tweets.txt - Example text file
+"""
+
+from tweetdeck_bot import tweetdeck
+
+# Set username and password
+bot = tweetdeck(username='placeholder', password='placeholder')
+
+# Start bot. Default values are just placeholders, change as you want.
+"""
+bot.start(starting_date='12-09-2019', FORMAT: MM-DD-YYYY
+          time_slots = [(8, 30, 'PM'), FORMAT: (hour, min, period) period is 'AM' or 'PM'
+                        (5, 30, 'PM'),
+                        (6, 30, 'PM'), 
+                        (7, 30, 'PM')],
+          source='tweets.txt') 
+          # Source file: file_name.txt, make sure to place the file in same folder, together with this
+          # file and tweetdeck_bot.py
+"""
+# EXAMPLE:
+bot.start(starting_date='12-09-2019',
+          time_slots = [(8, 30, 'PM'),
+                        (5, 30, 'PM'),
+                        (6, 30, 'PM'), 
+                        (7, 30, 'PM')],
+          source='tweets.txt')
+
